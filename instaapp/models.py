97ed3c42e44fa2b49c52276b973ccd8ecca5e01b
“Lissa <lwarshaw674@gmail.com>
2023-05-15 19:01:26 -0400

    init

---
+++ instaapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
+    caption = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.caption
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
